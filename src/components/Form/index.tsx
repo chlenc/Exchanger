ba88ac8a144f90ff94706175d91ca3c2ea9512f6
Alexey Nagorny <anagorny@wavesplatform.com>
2019-07-29 15:33:54 +0300

    make normal number input

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -9,8 +9,8 @@ const m = 1e8;
 
 interface IState {
     isWavesToken: boolean,
-    tokensCount: number,
-    minPrice: number
+    tokensCount: string,
+    minPrice: string
 }
 
 interface IProps {
@@ -24,15 +24,23 @@ export default class FreedForm extends React.Component<IProps, IState> {
 
     state: IState = {
         isWavesToken: true,
-        tokensCount: 0,
-        minPrice: 0
+        tokensCount: '',
+        minPrice: ''
     };
 
-    private handleChangeTokensCount = (e) => this.setState({tokensCount: +e.target.value});
-    private handleChangeMinPrice = (e) => this.setState({minPrice: +e.target.value});
+    private handleChangeTokensCount = (e) => {
+        if (!isNaN(+e.target.value)) {
+            this.setState({tokensCount: e.target.value});
+        }
+    };
+    private handleChangeMinPrice = (e) => {
+        if (!isNaN(+e.target.value)) {
+            this.setState({minPrice: e.target.value});
+        }
+    };
 
     private handleExchange = (minAmount: number) =>
-        this.props.dappStore!.exchange(this.state.tokensCount, this.state.isWavesToken, minAmount);
+        this.props.dappStore!.exchange(+this.state.tokensCount, this.state.isWavesToken, minAmount);
 
     private handleOnWavesToken = () => this.setState({isWavesToken: true});
 
@@ -44,28 +52,32 @@ export default class FreedForm extends React.Component<IProps, IState> {
     private get wavesAmount() {
         const {liquidAmount, wavesAmount} = this.props.dappStore!;
         const {tokensCount} = this.state;
-        return (liquidAmount * tokensCount) / (wavesAmount + tokensCount);
+        return (liquidAmount * (+tokensCount)) / ((+wavesAmount) + (+tokensCount));
     }
 
     private get liquidAmount() {
         const {liquidAmount, wavesAmount} = this.props.dappStore!;
         const {tokensCount} = this.state;
-        return (wavesAmount * tokensCount) / (liquidAmount + tokensCount);
+        return (wavesAmount * (+tokensCount)) / ((+liquidAmount) + (+tokensCount));
     }
 
     handleFocus = (e) => e.target.select();
 
     render(): React.ReactNode {
-        const {isWavesToken, tokensCount, minPrice} = this.state;
+        const isWavesToken = +this.state.isWavesToken;
+        const tokensCount = +this.state.tokensCount;
+        const minPrice = +this.state.minPrice;
         const {isApplicationAuthorizedInWavesKeeper: isLogin} = this.props.accountStore!;
         const payment = this.round8(isWavesToken ? this.wavesAmount : this.liquidAmount);
         let price = 0;
-        if (isWavesToken && payment !== 0 && tokensCount !== 0) {
+        if (isWavesToken && payment !== 0 && (+tokensCount) !== 0) {
             price = this.round8(tokensCount / payment);
         } else if (!isWavesToken && payment !== 0 && tokensCount !== 0) {
             price = this.round8(payment / tokensCount);
         }
-        const minAmount = isWavesToken ?( minPrice === 0 ? 0 : tokensCount / minPrice): ( minPrice === 0 ? 0 : tokensCount * minPrice) ;
+        const minAmount = isWavesToken
+            ? (minPrice === 0 ? 0 : tokensCount / minPrice)
+            : (minPrice === 0 ? 0 : tokensCount * minPrice);
         return <div className={styles.root}>
 
             <div>
@@ -89,42 +101,41 @@ export default class FreedForm extends React.Component<IProps, IState> {
                         </button>
                     </div>
                 </div>
-                    <div className={styles.borderBottom}>
-                        <div className={styles.calculateField_col}>
-                            <div className={styles.header2Font}>
-                                Exchange amount
+                <div className={styles.borderBottom}>
+                    <div className={styles.calculateField_col}>
+                        <div className={styles.header2Font}>
+                            Exchange amount
+                        </div>
+                        <div className={styles.captionFont}>You pay</div>
+                        <div className={styles.inputField}>
+                            {isWavesToken ? <div className={styles.wavesIcn}/> : <div className={styles.btcIcn}/>}
+                            <input
+                                className={styles.input}
+                                onChange={this.handleChangeTokensCount}
+                                onFocus={this.handleFocus}
+                                value={this.state.tokensCount}
+                            />
+                        </div>
+                        <br/>
+                        <div className={styles.rateField_row}>
+                            Price:
+                            <div className={styles.rateFont}>
+                                <b className={styles.rateCount}>~&nbsp;{price}</b> &nbsp;
+                                <div className={styles.rateFont_btc}>LIQUID</div>
+                                &nbsp;/&nbsp;
+                                <div className={styles.rateFont_waves}>WAVES</div>
                             </div>
-                            <div className={styles.captionFont}>You pay</div>
-                            <div className={styles.inputField}>
-                                {isWavesToken ? <div className={styles.wavesIcn}/> : <div className={styles.btcIcn}/>}
-                                <input
-                                    className={styles.input}
-                                    type="number"
-                                    onChange={this.handleChangeTokensCount}
-                                    onFocus={this.handleFocus}
-                                    value={tokensCount}
-                                />
-                            </div>
-                            <br/>
-                            <div className={styles.rateField_row}>
-                                Price:
-                                <div className={styles.rateFont}>
-                                    <b className={styles.rateCount}>~&nbsp;{price}</b> &nbsp;
-                                    <div className={styles.rateFont_btc}>LIQUID</div>
-                                    &nbsp;/&nbsp;
+                        </div>
+                        <div className={styles.rateField_row}>
+                            You get:
+                            <div className={styles.rateFont}>
+                                <b className={styles.rateCount}>~&nbsp;{payment}</b> &nbsp;
+                                {isWavesToken ?
+                                    <div className={styles.rateFont_btc}>LIQUID</div> :
                                     <div className={styles.rateFont_waves}>WAVES</div>
-                                </div>
+                                }
                             </div>
-                            <div className={styles.rateField_row}>
-                                You get:
-                                <div className={styles.rateFont}>
-                                    <b className={styles.rateCount}>~&nbsp;{payment}</b> &nbsp;
-                                    {isWavesToken ? 
-                                     <div className={styles.rateFont_btc}>LIQUID</div>:
-                                     <div className={styles.rateFont_waves}>WAVES</div>
-                                    }
-                                </div>
-                            </div>
+                        </div>
 
                     </div>
 
@@ -137,10 +148,9 @@ export default class FreedForm extends React.Component<IProps, IState> {
                 <div className={styles.inputField}>
                     <input
                         className={styles.min}
-                        type="number"
                         onChange={this.handleChangeMinPrice}
                         onFocus={this.handleFocus}
-                        value={minPrice}
+                        value={this.state.minPrice}
                     />
                 </div>
                 <br/>
