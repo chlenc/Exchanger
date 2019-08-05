f546ab2b1a013cc2e762e5e4fa89aed6ef11692d
Alexey Nagorny <anagorny@wavesplatform.com>
2019-08-05 13:08:08 +0300

    Updates

---
+++ precache-manifest.7bb1fc9d159c94cf33761ba5ce61a203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10c88ae096967a719ad0cb29932c1309",
+    "url": "/lwexch/static/media/GIF1.10c88ae0.gif"
+  },
+  {
+    "revision": "0407af758ab58371be6f",
+    "url": "/lwexch/static/js/runtime~main.0407af75.js"
+  },
+  {
+    "revision": "ba59db1e75529a94ebac",
+    "url": "/lwexch/static/js/main.ba59db1e.chunk.js"
+  },
+  {
+    "revision": "b0981d0f9f539aedbae4",
+    "url": "/lwexch/static/js/1.b0981d0f.chunk.js"
+  },
+  {
+    "revision": "ba59db1e75529a94ebac",
+    "url": "/lwexch/static/css/main.872ae191.chunk.css"
+  },
+  {
+    "revision": "b0981d0f9f539aedbae4",
+    "url": "/lwexch/static/css/1.24310a58.chunk.css"
+  },
+  {
+    "revision": "ca4676106b27606b4d00ee61c744b364",
+    "url": "/lwexch/index.html"
+  }
+];
